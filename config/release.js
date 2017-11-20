240737301e04aaa2b3eb071e51c86883f9754c60
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-11-20 10:41:33 +0100

    Add ember-cli-release for releasing versions

---
+++ config/release.js
@@ -0,0 +1,20 @@
+/* eslint-env node */
+// const RSVP = require('rsvp');
+
+module.exports = {
+  // local: true,
+  // remote: 'some_remote',
+  // annotation: "Release %@",
+  // message: "Bumped version to %@",
+  // manifest: [ 'package.json', 'bower.json', 'someconfig.json' ],
+  // publish: true,
+  // strategy: 'date',
+  // format: 'YYYY-MM-DD',
+  // timezone: 'America/Los_Angeles',
+  //
+  // beforeCommit: function(project, versions) {
+  //   return new RSVP.Promise(function(resolve, reject) {
+  //     // Do custom things here...
+  //   });
+  // }
+}
