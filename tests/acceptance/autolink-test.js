37eb2df9b605573e65d42be506429f1ce9bdf05f
Jonas Metzener <jonas.metzener@adfinis-sygroup.ch>
2017-11-20 10:15:13 +0100

    Enforce using semicolons

--- tests/acceptance/autolink-test.js
+++ tests/acceptance/autolink-test.js
@@ -1,52 +1,52 @@
-import { describe, it, beforeEach, afterEach } from 'mocha'
-import { expect } from 'chai'
-import startApp from '../helpers/start-app'
-import destroyApp from '../helpers/destroy-app'
-import { findAll, fillIn } from 'ember-native-dom-helpers'
+import { describe, it, beforeEach, afterEach } from 'mocha';
+import { expect } from 'chai';
+import startApp from '../helpers/start-app';
+import destroyApp from '../helpers/destroy-app';
+import { findAll, fillIn } from 'ember-native-dom-helpers';
 
 describe('Acceptance | autolink', function() {
-  let application
+  let application;
 
   beforeEach(function() {
-    application = startApp()
-  })
+    application = startApp();
+  });
 
   afterEach(function() {
-    destroyApp(application)
-  })
+    destroyApp(application);
+  });
 
   it('can use autolink helper with escaping', function() {
-    visit('/')
+    visit('/');
 
     andThen(() => {
       fillIn(
         '.example textarea',
         'Your text containing URLs like google.com or http://www.something-else.xyz?whatever=you#like'
-      )
-    })
+      );
+    });
 
     andThen(() => {
-      expect(findAll('.example a')).to.have.length(2)
-    })
-  })
+      expect(findAll('.example a')).to.have.length(2);
+    });
+  });
 
   it('can use autolink helper without escaping', function() {
-    visit('/')
+    visit('/');
 
     andThen(() => {
-      click('.example input')
-    })
+      click('.example input');
+    });
 
     andThen(() => {
       fillIn(
         '.example textarea',
         'fancy html<br><strong>with emberjs.com</strong>'
-      )
-    })
+      );
+    });
 
     andThen(() => {
-      expect(findAll('.example strong')).to.have.length(1)
-      expect(findAll('.example a')).to.have.length(1)
-    })
-  })
-})
+      expect(findAll('.example strong')).to.have.length(1);
+      expect(findAll('.example a')).to.have.length(1);
+    });
+  });
+});
--- tests/integration/helpers/autolink-test.js
+++ tests/integration/helpers/autolink-test.js
@@ -1,46 +1,46 @@
-import { expect } from 'chai'
-import { describe, it } from 'mocha'
-import { setupComponentTest } from 'ember-mocha'
-import hbs from 'htmlbars-inline-precompile'
-import { find, findAll } from 'ember-native-dom-helpers'
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { setupComponentTest } from 'ember-mocha';
+import hbs from 'htmlbars-inline-precompile';
+import { find, findAll } from 'ember-native-dom-helpers';
 
 describe('Integration | Helper | autolink', function() {
   setupComponentTest('autolink', {
     integration: true
-  })
+  });
 
   it('creates links with escaping', function() {
     this.set(
       'text',
       'Hello test, let me google that for you: http://lmgtfy.com/?q=ember+testing'
-    )
+    );
 
-    this.render(hbs`{{autolink text}}`)
+    this.render(hbs`{{autolink text}}`);
 
-    expect(findAll('a')).to.have.length(1)
+    expect(findAll('a')).to.have.length(1);
 
-    expect(find('a').href).to.equal('http://lmgtfy.com/?q=ember+testing')
-  })
+    expect(find('a').href).to.equal('http://lmgtfy.com/?q=ember+testing');
+  });
 
   it('creates links without escaping', function() {
     this.set(
       'text',
       'Hello test, let me <strong>google</strong> that for you: http://lmgtfy.com/?q=ember+testing'
-    )
+    );
 
-    this.render(hbs`{{autolink text true}}`)
+    this.render(hbs`{{autolink text true}}`);
 
-    expect(findAll('strong')).to.have.length(1)
-    expect(findAll('a')).to.have.length(1)
+    expect(findAll('strong')).to.have.length(1);
+    expect(findAll('a')).to.have.length(1);
 
-    expect(find('a').href).to.equal('http://lmgtfy.com/?q=ember+testing')
-  })
+    expect(find('a').href).to.equal('http://lmgtfy.com/?q=ember+testing');
+  });
 
   it('creates multiple links', function() {
-    this.set('text', 'https://google.com is way better than https://bing.com!')
+    this.set('text', 'https://google.com is way better than https://bing.com!');
 
-    this.render(hbs`{{autolink text}}`)
+    this.render(hbs`{{autolink text}}`);
 
-    expect(findAll('a')).to.have.length(2)
-  })
-})
+    expect(findAll('a')).to.have.length(2);
+  });
+});
